d0a69837d908d7b5df506eb8fbabe1402157085a
Bruno Takahashi <bruno.kazuo90@gmail.com>
2020-05-15 18:19:45 -0400

    Creating custom exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,61 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($request->wantsJson()) {   //add Accept: application/json in request
+
+            if ($exception instanceof \Illuminate\Http\Exception\HttpResponseException) {
+                $exception = $exception->getResponse();
+            }
+
+            if ($exception instanceof \Illuminate\Auth\AuthenticationException) {
+                $exception = $this->unauthenticated($request, $exception);
+            }
+
+            if ($exception instanceof \Illuminate\Validation\ValidationException) {
+                $exception = $this->convertValidationExceptionToResponse($exception, $request);
+            }
+
+            return $this->customApiResponse($exception);
+        } else {
+            $result = parent::render($request, $exception);
+        }
+
+        return $result;
+    }
+
+    private function customApiResponse($exception) {
+        if (method_exists($exception, 'getStatusCode')) {
+            $statusCode = $exception->getStatusCode();
+        } else {
+            $statusCode = 500;
+        }
+
+        $response = [];
+
+        switch ($statusCode) {
+            case 401:
+                $response['message'] = 'Unauthorized';
+                break;
+            case 403:
+                $response['message'] = 'Forbidden';
+                break;
+            case 404:
+                $response['message'] = 'Not Found';
+                break;
+            case 405:
+                $response['message'] = 'Method Not Allowed';
+                break;
+            case 422:
+                $response['message'] = $exception->original['message'];
+                $response['errors'] = $exception->original['errors'];
+                break;
+            default:
+                $response['message'] = ($statusCode == 500) ? 'Whoops, looks like something went wrong' : $exception->getMessage();
+                break;
+        }
+
+        $response['status'] = $statusCode;
+
+        return response()->json($response, $statusCode);
     }
 }
